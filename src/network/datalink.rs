805353b6ef9dbfbca7afc29d37b283c4f23c26fa
Max Kofler <kofler.max.dev@gmail.com>
2023-09-06 21:10:15 +0200

    Update frame layout

--- src/network/datalink.rs
+++ src/network/datalink.rs
@@ -1,8 +1,12 @@
-use crc::{Crc, CRC_32_BZIP2};
+use crc::{Crc, CRC_32_BZIP2, CRC_8_BLUETOOTH};
 use embedded_hal::serial::Write;
 use nb::block;
 
-const CRC_ALGORITHM: Crc<u32> = Crc::<u32>::new(&CRC_32_BZIP2);
+const F_CRC_ALGORITHM: Crc<u32> = Crc::<u32>::new(&CRC_32_BZIP2);
+const H_CRC_ALGORITHM: Crc<u8> = Crc::<u8>::new(&CRC_8_BLUETOOTH);
+
+const START_BYTE_0: u8 = 0xaa;
+const START_BYTE_1: u8 = 0x55;
 
 /// A frame from the Data Link Layer
 #[derive(Clone)]
@@ -13,10 +17,12 @@ pub struct DataFrame {
     pub dst: u16,
     /// The amount of bytes in the payload
     pub payload_len: u8,
+    /// The header CRC
+    pub h_crc: u8,
     /// The payload itself
     pub payload: [u8; u8::MAX as usize + 1],
     /// The CRC of the frame
-    pub crc: u32,
+    pub f_crc: u32,
     /// (internal) The incoming length of an unassembled frame
     in_len: u16,
 }
@@ -30,10 +36,13 @@ pub struct DataLinkLayer {
 impl DataFrame {
     /// Generates a CRC32 - BZIP2 for this DataFrame
     pub fn crc(&self) -> u32 {
-        let mut digest = CRC_ALGORITHM.digest();
+        let mut digest = F_CRC_ALGORITHM.digest();
+
+        digest.update(&[START_BYTE_0, START_BYTE_1]);
         digest.update(&[(self.src & 0xff) as u8, (self.src >> 8 & 0xff) as u8]);
         digest.update(&[(self.dst & 0xff) as u8, (self.dst >> 8 & 0xff) as u8]);
         digest.update(&[self.payload_len]);
+        digest.update(&[self.h_crc]);
 
         for pos in 0..self.payload_len {
             digest.update(&[self.payload[pos as usize]]);
@@ -42,16 +51,28 @@ impl DataFrame {
         digest.finalize()
     }
 
+    pub fn h_crc(&self) -> u8 {
+        let mut digest = H_CRC_ALGORITHM.digest();
+
+        digest.update(&[START_BYTE_0, START_BYTE_1]);
+        digest.update(&[(self.src & 0xff) as u8, (self.src >> 8 & 0xff) as u8]);
+        digest.update(&[(self.dst & 0xff) as u8, (self.dst >> 8 & 0xff) as u8]);
+        digest.update(&[self.payload_len]);
+
+        digest.finalize()
+    }
+
     /// Updates the internal CRC to the appropriate value for the frame in this state
     pub fn update_crc(&mut self) {
-        self.crc = self.crc();
+        self.h_crc = self.h_crc();
+        self.f_crc = self.crc();
     }
 
     /// Checks if the received CRC and the calculated CRC are the same
     /// # Returns
     /// `true` if the CRC is valid, else `false`
     pub fn check_crc(&self) -> bool {
-        self.crc == self.crc()
+        self.f_crc == self.crc()
     }
 
     /// Moves this value, checks if the CRC is valid and returns the frame, else discards it
@@ -86,8 +107,8 @@ impl DataFrame {
         self.update_crc();
 
         // Write start bytes
-        block!(serial.write(0xaa))?;
-        block!(serial.write(0x55))?;
+        block!(serial.write(START_BYTE_0))?;
+        block!(serial.write(START_BYTE_1))?;
 
         // Write src address
         block!(serial.write((self.src & 0xff) as u8))?;
@@ -97,17 +118,22 @@ impl DataFrame {
         block!(serial.write((self.dst & 0xff) as u8))?;
         block!(serial.write((self.dst >> 8 & 0xff) as u8))?;
 
-        // Write payload
+        // Write payload len
         block!(serial.write(self.payload_len))?;
+
+        // Write header crc
+        block!(serial.write(self.h_crc))?;
+
+        // Write payload
         for i in 0..self.payload_len {
             block!(serial.write(self.payload[i as usize]))?;
         }
 
         // Write CRC
-        block!(serial.write((self.crc & 0xff) as u8))?;
-        block!(serial.write((self.crc >> 8 & 0xff) as u8))?;
-        block!(serial.write((self.crc >> 16 & 0xff) as u8))?;
-        block!(serial.write((self.crc >> 24 & 0xff) as u8))?;
+        block!(serial.write((self.f_crc & 0xff) as u8))?;
+        block!(serial.write((self.f_crc >> 8 & 0xff) as u8))?;
+        block!(serial.write((self.f_crc >> 16 & 0xff) as u8))?;
+        block!(serial.write((self.f_crc >> 24 & 0xff) as u8))?;
 
         Ok(())
     }
@@ -155,20 +181,30 @@ impl DataLinkLayer {
                 // len
                 self.cur_frame.payload_len = data;
             }
+            7 => {
+                // Check if the header is valid, else drop the frame
+                if data != self.cur_frame.h_crc() {
+                    self.reset();
+                    return None;
+                }
+
+                // h_crc
+                self.cur_frame.h_crc = data;
+            }
             _ => {
                 // The index of the last payload byte
-                let payload_last = 6 + self.cur_frame.payload_len as u16;
+                let payload_last = 7 + self.cur_frame.payload_len as u16;
                 // The index of the last CRC byte
                 let crc_last = payload_last + 4;
 
                 if self.cur_frame.in_len <= payload_last {
                     //Payload
-                    self.cur_frame.payload[self.cur_frame.in_len as usize - 7] = data;
+                    self.cur_frame.payload[self.cur_frame.in_len as usize - 8] = data;
                 } else if self.cur_frame.in_len <= crc_last {
                     //CRC
                     let crc_pos =
-                        self.cur_frame.in_len as usize - 7 - self.cur_frame.payload_len as usize;
-                    self.cur_frame.crc |= (data as u32) << (crc_pos * 8);
+                        self.cur_frame.in_len as usize - 8 - self.cur_frame.payload_len as usize;
+                    self.cur_frame.f_crc |= (data as u32) << (crc_pos * 8);
                 }
 
                 // This marks the end of a frame
@@ -201,8 +237,9 @@ impl Default for DataFrame {
             src: 0,
             dst: 0,
             payload_len: 0,
+            h_crc: 0,
             payload: [0; u8::MAX as usize + 1],
-            crc: 0,
+            f_crc: 0,
             in_len: 0,
         }
     }
