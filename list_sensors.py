9f7fdcd5ef3f1516fbc2182700ab999ba535dc06
Max Kofler <kofler.max.dev@gmail.com>
2023-09-09 08:26:53 +0200

    Update python scripts

---
+++ list_sensors.py
@@ -0,0 +1,59 @@
+'''Test script'''
+#!python3
+
+import sys
+
+import struct
+import serial
+
+from python.frame import Frame, frame_decode, ExpectedBytesCountError
+
+if len(sys.argv) <= 2:
+    print("Usage: <serial port> <address>")
+    exit(-1)
+
+ser = serial.Serial(sys.argv[1], baudrate=57600)
+ser.timeout = 1
+
+CLIENT_ADDR = int(sys.argv[2], 0)
+print(f"Talking to {hex(CLIENT_ADDR)}")
+
+def get_frame(serial_port: serial.Serial, cmd: int, payload: bytes):
+    '''Sond'''
+    send_frame = Frame(0x0000, CLIENT_ADDR, cmd, payload)
+    serial_port.write(send_frame.to_bytes())
+    return frame_decode(serial_port)
+
+num_sensors: int = 0
+try:
+    num_sensors = int.from_bytes(get_frame(ser, 0x0100, bytes()).payload, byteorder="little")
+except ExpectedBytesCountError:
+    print(f"Client {hex(CLIENT_ADDR)} seems offline / not available")
+    exit(0)
+print(f"{num_sensors} available sensors")
+print("")
+
+for i in range(0, num_sensors):
+    s_id_bytes = i.to_bytes(4, byteorder="little")
+
+    unique_id = get_frame(ser, 0x0102, s_id_bytes).payload.decode()
+    native_unit_of_measurement = get_frame(ser, 0x0104, s_id_bytes).payload.decode()
+    device_class = get_frame(ser, 0x0106, s_id_bytes).payload.decode()
+    state_class = get_frame(ser, 0x0108, s_id_bytes).payload.decode()
+    name = get_frame(ser, 0x0110, s_id_bytes).payload.decode()
+    value = get_frame(ser, 0x0112, s_id_bytes).payload
+
+    if value[0] == 2:
+        value = struct.unpack("f", value[1:5])[0]
+    elif value[0] == 1:
+        value = int.from_bytes(value[1:5], byteorder="little")
+    elif value[0] == 0:
+        value = value[1:].decode()
+
+    print(f"Sensor {i}:")
+    print(f"    name:                       {name}")
+    print(f"    unique_id:                  {unique_id}")
+    print(f"    value:                      {value}")
+    print(f"    native_unit_of_measurement: {native_unit_of_measurement}")
+    print(f"    device_class:               {device_class}")
+    print(f"    state_class:                {state_class}")
---
+++ python/frame.py
@@ -0,0 +1,144 @@
+'''The DataLink layer'''
+
+import serial
+
+from crc import Calculator, Crc32, Crc8
+START_BYTE_0 = 0xaa
+START_BYTE_1 = 0x55
+
+class Frame:
+    '''A frame in the datalink layer'''
+
+    def __init__(self, src: int, dst: int, cmd: int, payload: bytes):
+        '''Create new frame'''
+
+        self.src = src
+        self.dst = dst
+        self.cmd = cmd
+        self.payload = payload
+
+    def to_bytes(self) -> bytes:
+        '''Generate a byte array from this frame for transmission'''
+
+        frame_bytes = bytearray()
+
+        frame_bytes += START_BYTE_0.to_bytes(1, byteorder="little")
+        frame_bytes += START_BYTE_1.to_bytes(1, byteorder="little")
+        frame_bytes += self.src.to_bytes(2, byteorder="little")
+        frame_bytes += self.dst.to_bytes(2, byteorder="little")
+        frame_bytes += self.cmd.to_bytes(2, byteorder="little")
+        frame_bytes += len(self.payload).to_bytes(1, byteorder="little")
+
+        header_calculator = Calculator(Crc8.BLUETOOTH)
+        header_crc = header_calculator.checksum(frame_bytes)
+
+        frame_bytes += header_crc.to_bytes(1, byteorder="little")
+        frame_bytes += self.payload
+
+        frame_calculator = Calculator(Crc32.BZIP2)
+        frame_crc = frame_calculator.checksum(frame_bytes)
+
+        frame_bytes += frame_crc.to_bytes(4, byteorder="little")
+
+        return bytes(frame_bytes)
+
+class ExpectedBytesCountError(Exception):
+    '''
+    An error that gets raised if the expected amount of bytes
+    wasn't fulfilled
+    '''
+    def __init__(self, expected: int, got: int, reason: str):
+        self.expected = expected
+        self.got = got
+        self.message = f"Expected {self.expected} bytes {reason}, got {self.got}"
+        super().__init__(self.message)
+
+class StartBytesError(Exception):
+    '''
+    An error that gets raised if the start bytes weren't the first bytes
+    '''
+    def __init__(self, got: bytes):
+        self.got = got
+        self.message = f"Expected start bytes, got {self.got}"
+        super().__init__(self.message)
+
+class HeaderCRCError(Exception):
+    '''
+    An error that gets raised if the header crc did not match
+    '''
+    def __init__(self, expected: int, got: int):
+        self.expected = expected
+        self.got = got
+        self.message = f"Expected header crc {self.expected}, got {self.got}"
+        super().__init__(self.message)
+
+class FrameCRCError(Exception):
+    '''
+    An error that gets raised if the frame crc did not match
+    '''
+    def __init__(self, expected: int, got: int):
+        self.expected = expected
+        self.got = got
+        self.message = f"Expected frame crc {self.expected}, got {self.got}"
+        super().__init__(self.message)
+
+def rec_bytes(count: int, ser: serial.Serial, reason: str) -> bytes:
+    '''Receive n bytes from the serial instance'''
+
+    in_bytes = ser.read(count)
+
+    if not len(in_bytes) == count:
+        raise ExpectedBytesCountError(count, len(in_bytes), reason)
+
+    return in_bytes
+
+def frame_decode(ser: serial.Serial) -> Frame:
+    '''Decode a frame from a serial interface'''
+
+    start_bytes = bytes([START_BYTE_0, START_BYTE_1])
+
+    rec_start_bytes = rec_bytes(2, ser, "for start bytes")
+    if not rec_start_bytes == start_bytes:
+        raise StartBytesError(rec_start_bytes)
+
+    received_bytes = bytearray(start_bytes)
+
+    b_src = rec_bytes(2, ser, "for src")
+    received_bytes += b_src
+    src = int.from_bytes(b_src, byteorder="little")
+
+    b_dst = rec_bytes(2, ser, "for dst")
+    received_bytes += b_dst
+    dst = int.from_bytes(b_dst, byteorder="little")
+
+    b_cmd = rec_bytes(2, ser, "for cmd")
+    received_bytes += b_cmd
+    cmd = int.from_bytes(b_cmd, byteorder="little")
+
+    b_payload_len = rec_bytes(1, ser, "for payload len")
+    received_bytes += b_payload_len
+    payload_len = int.from_bytes(b_payload_len, byteorder="little")
+
+    b_header_crc = rec_bytes(1, ser, "for header crc")
+    header_crc = int.from_bytes(b_header_crc, byteorder="little")
+
+    header_calculator = Calculator(Crc8.BLUETOOTH)
+    calculated_header_crc = header_calculator.checksum(received_bytes)
+
+    if not calculated_header_crc == header_crc:
+        raise HeaderCRCError(calculated_header_crc, header_crc)
+    received_bytes += b_header_crc
+
+    payload = rec_bytes(payload_len, ser, "for payload")
+    received_bytes += payload
+
+    b_frame_crc = rec_bytes(4, ser, "for frame crc")
+    frame_crc = int.from_bytes(b_frame_crc, byteorder="little")
+
+    frame_calculator = Calculator(Crc32.BZIP2)
+    calculated_frame_crc = frame_calculator.checksum(received_bytes)
+
+    if not calculated_frame_crc == frame_crc:
+        raise FrameCRCError(calculated_frame_crc, frame_crc)
+
+    return Frame(src, dst, cmd, payload)
