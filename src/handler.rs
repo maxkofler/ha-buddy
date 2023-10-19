ce7f1e3317511d5afa87fcbeb58f205f5c0b256d
Max Kofler <kofler.max.dev@gmail.com>
2023-10-19 21:22:38 +0200

    Update handler for new DataFrame management

--- src/handler.rs
+++ src/handler.rs
@@ -1,181 +1,181 @@
-use crate::{network::DataFrame, sensor::SensorRef};
+use crate::{sensor::SensorRef, DataFrame};
 
 pub struct HandlerPins {}
 
-/// Handles an incoming frame
+/// Handles an incoming frame and possibly mutates the incoming frame for a response.
+///
+/// The `frame` argument gets mutated and prepared as the response structure.
+/// If this function returns true, it shall be transmitted
 /// # Arguments
-/// * `frame` - The frame to process
+/// * `frame` - The frame to process and mutate for responses
 /// * `pins` - Pins that are exposed for the handler
 /// * `sensors` - The sensors to handle
 /// # Returns
-/// A `DataFrame` as response, else None
+/// True if the modified frame is to be sent
 pub fn handle_frame(
-    frame: DataFrame,
-    pins: &mut HandlerPins,
+    frame: &mut DataFrame,
+    _pins: &mut HandlerPins,
     sensors: &[&dyn SensorRef],
-) -> Option<DataFrame> {
-    let mut res = DataFrame::default();
-    res.cmd = frame.cmd + 1;
+) -> bool {
     match frame.cmd {
         0x0000 => {
             // Echo
-            for i in 0..(frame.payload_len as usize) {
-                res.payload[i] = frame.payload[i]
-            }
 
-            Some(res)
+            // We do not do anything, the payload stays the same
+
+            true
         }
         0x0100 => {
             // sensor count
             let num_sensors = sensors.len() as u32;
 
-            res.payload_len = 4;
+            frame.payload_len = 4;
 
-            res.payload[0] = num_sensors as u8 & 0xff;
-            res.payload[1] = (num_sensors >> 8) as u8 & 0xff;
-            res.payload[2] = (num_sensors >> 16) as u8 & 0xff;
-            res.payload[3] = (num_sensors >> 24) as u8 & 0xff;
+            frame.payload[0] = num_sensors as u8 & 0xff;
+            frame.payload[1] = (num_sensors >> 8) as u8 & 0xff;
+            frame.payload[2] = (num_sensors >> 16) as u8 & 0xff;
+            frame.payload[3] = (num_sensors >> 24) as u8 & 0xff;
 
-            Some(res)
+            true
         }
         0x0102 => {
             // Sensor unique_id
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
             let string = sensors[sensor_id as usize].get_unique_id();
 
-            res.payload_len = string.len() as u8;
+            frame.payload_len = string.len() as u8;
             let bytes = string.as_bytes();
 
-            for i in 0..(res.payload_len as usize) {
-                res.payload[i] = bytes[i];
+            for i in 0..(frame.payload_len as usize) {
+                frame.payload[i] = bytes[i];
             }
 
-            Some(res)
+            true
         }
         0x0104 => {
             // Sensor native_unit_of_measurement
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
             let string = sensors[sensor_id as usize].get_native_unit_of_measurement();
 
-            res.payload_len = string.len() as u8;
+            frame.payload_len = string.len() as u8;
             let bytes = string.as_bytes();
 
-            for i in 0..(res.payload_len as usize) {
-                res.payload[i] = bytes[i];
+            for i in 0..(frame.payload_len as usize) {
+                frame.payload[i] = bytes[i];
             }
 
-            Some(res)
+            true
         }
         0x0106 => {
             // Sensor device_class
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
             let string = sensors[sensor_id as usize].get_device_class().as_str();
 
-            res.payload_len = string.len() as u8;
+            frame.payload_len = string.len() as u8;
             let bytes = string.as_bytes();
 
-            for i in 0..(res.payload_len as usize) {
-                res.payload[i] = bytes[i];
+            for i in 0..(frame.payload_len as usize) {
+                frame.payload[i] = bytes[i];
             }
 
-            Some(res)
+            true
         }
         0x0108 => {
             // Sensor state_class
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
             let string = sensors[sensor_id as usize].get_state_class().as_str();
 
-            res.payload_len = string.len() as u8;
+            frame.payload_len = string.len() as u8;
             let bytes = string.as_bytes();
 
-            for i in 0..(res.payload_len as usize) {
-                res.payload[i] = bytes[i];
+            for i in 0..(frame.payload_len as usize) {
+                frame.payload[i] = bytes[i];
             }
 
-            Some(res)
+            true
         }
         0x0110 => {
             // Sensor device_class
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
             let string = sensors[sensor_id as usize].get_name();
 
-            res.payload_len = string.len() as u8;
+            frame.payload_len = string.len() as u8;
             let bytes = string.as_bytes();
 
-            for i in 0..(res.payload_len as usize) {
-                res.payload[i] = bytes[i];
+            for i in 0..(frame.payload_len as usize) {
+                frame.payload[i] = bytes[i];
             }
 
-            Some(res)
+            true
         }
         0x0112 => {
             // Sensor value
 
             let sensor_id: u32 = match unpack_u32(&frame.payload[0..4]) {
-                None => return None,
+                None => return false,
                 Some(id) => id,
             };
 
             if sensor_id as usize >= sensors.len() {
-                res.payload_len = 0;
-                return Some(res);
+                frame.payload_len = 0;
+                return true;
             }
 
-            sensors[sensor_id as usize].get_payload(&mut res.payload_len, &mut res.payload);
+            sensors[sensor_id as usize].get_payload(&mut frame.payload_len, &mut frame.payload);
 
-            Some(res)
+            true
         }
-        _ => None,
+        _ => false,
     }
 }
 
